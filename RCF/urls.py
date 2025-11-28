61eab0b66e168e794b0321068bde4c4a3634e76d
savyjeremiah <saviourjeremiahetim@gmail.com>
2025-11-28 12:27:41 +0100

    second

--- RCF/urls.py
+++ RCF/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include  # include lets us reference app URLs
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('admin/', admin.site.urls),       # Admin panel
+    path('', include('myapp.urls')),       # Routes all other URLs to myapp
 ]
+
